c8083f55b15198b4a33636c86d2ca4b6c680c687
moruowait <285599633@qq.com>
2019-08-19 16:58:05 +0800

    use log

--- examples/google_cloud_function/function.go
+++ examples/google_cloud_function/function.go
@@ -3,24 +3,72 @@ package p
 
 import (
 	"encoding/json"
-	"fmt"
-	"html"
+	"io"
+	"log"
 	"net/http"
+	"regexp"
+	"unicode"
 )
 
-// HelloWorld prints the JSON encoded "message" field in the body
-// of the request or "Hello, World!" if there isn't one.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
+type webhook struct {
+	PullRequest pullRequest
+	Body        string
+}
+
+type pullRequest struct {
+	Title string
+}
+
+// ReportPRValidationStatus check whether PR title and description is valid and report status to GitHub.
+func ReportPRValidationStatus(w http.ResponseWriter, r *http.Request) {
+
+	var wh webhook
+	if err := json.NewDecoder(r.Body).Decode(&wh); err != nil {
+		log.Printf("Failed to decode requestBody: %v", err)
 		return
 	}
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
+	if !isTitleValid(wh.PullRequest.Title) {
+		io.WriteString(w, "OK")
+		// report status
 		return
 	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	if isBodyValid(wh.Body) {
+		io.WriteString(w, "OK")
+		// report status
+		return
+	}
+	log.Printf("receive: %v", &wh)
+	// report status
+	io.WriteString(w, "OK")
+}
+
+var scopeRe = regexp.MustCompile(`^[\/\w]+:\s+`)
+var formatRe = regexp.MustCompile(`：|\s{2}|^.*[^\w]$`)
+
+func isTitleValid(s string) bool {
+	for _, r := range s {
+		if unicode.In(r, unicode.Han, unicode.Latin) || unicode.IsPunct(r) || r == ' ' {
+			continue
+		} else {
+			return false
+		}
+	}
+	if formatRe.Match([]byte(s)) {
+		return false
+	}
+	if !scopeRe.Match([]byte(s)) {
+		return false
+	}
+	return true
+}
+
+func isBodyValid(s string) bool {
+	for _, r := range s {
+		if unicode.In(r, unicode.Han, unicode.Latin) || unicode.IsPunct(r) || unicode.IsSpace(r) {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
 }
