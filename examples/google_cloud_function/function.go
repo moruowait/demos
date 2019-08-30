80be6398e2110d0264c48c4d24d397ce1104b53b
moruowait <285599633@qq.com>
2019-08-30 14:40:07 +0800

    sort

--- examples/google_cloud_function/function.go
+++ examples/google_cloud_function/function.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rsa"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -30,13 +31,13 @@ const (
 	statusContext   = "Title and description"
 	statusTargetURL = "https://github.com/xreception/depot/wiki/Pull-Request-Title-and-Description"
 
-	// encryptedGitHubToken = "CiQA0ev1XJIfg6wmsY7Ln3bbmVqIZVRN0hdsn/L5HXRfb7wTAboSUQB15DKoxvXMvb9vrqco3JVZ46Mw7fBbSBAMCwKD6R33kd0MKQ1QYBsYSeGezwIlk83SwXbqhWZYBLEHgsIJ6mdPFrx76NVf+6x5l5hdxhhlhw=="
 	encryptedPEM = "CiQA0ev1XM9ZJ03VP0YPwVfb+4+mP+PZXUJg3tLZdnB5c4Un334SuQ0AdeQyqHdLFY5yaOOcFArPSHtrjToQ1TpsNTkRuB6EmW0C/AUzzJw4/vcZE8hiG7qIYklneimtU0d47PtDqytAFq0fs0nNwALlFbuqgywoSw1Gogz6zmJ12sMXMmmSA+qd79iOPvUAekhiuDxrF8+FPwoFTfZ5bMafTYbKxkxXWQicY/te+PmmT2ncVwKps7Xk/GxDrXhvXrf32H2RF0b9E4TyKNRDjxF+Ljt0ZQIezA4hpbxgHL+ng0+YuyeTRyRQh8zaVsBa/ELg72F4y0mcxtVTFREKFxKR3GBYZ5fErEUCFLnZuRiDKUWzt+uY01ZHqMak+Am9cxn+1DwbL9xtXPrNd+DgSuzkID2MH9WY31XR7EHnOi6Y5FRsOO43HbFdppLUKwwC9UQgQp41RDtAkU85eDnap0RJSWIjYBjDnS7fc/jMZTmGA26qWhcCK9lYbgeagz7CxDG8iwbi9XPMKzAj/NCRi9coV71RJKmZfUCE8TLSpGMb3+YRrtk7PB46zctOiHP1KkWdqre3QJllB5lrEtWJJ9gkeZlYIEGYd5LIbOsgVsMb0WDJYi49N8y7y86XpRBBtL9Sy5dzCX8uay/qR2Z39zYJhoM9E7XhWXWsNgDWJt1jT+WGmzogWSsL20KCS1Q4JgtvatiJUP7ueKSEKX3fZmZTcp84Ux1XR28g5F7to2/jByZbPCkl0MTt7WaFCv8QCCP3Cv4h0PBA7dpJdCg8RWwHAj6VJAS0PC0GZClPOfmzQM5BNLhdjV5L2dLcYOJBlWBBK2wOJDFsSQdgh170OdkGD3bwMhF3pqvophg8BzwF93TgIq2k9nxdS3Yc0PYxyLQb33mtAQS4VBsdIEqcgrcYDJVYfU/hZ2IgThZP73ZB35LYZ4BcMeO0PEmiv+ps14DyXaROKCvYCtdBQLVKk2l14MQNmpGh6xmso5j6244PqcqbKcAigI8Z7zBSe/97L1g+7H2sH82lKMzAp/WvfLLQW9tLmv5l6/NeTm+p4+eWI3fMYfh77/G3VDwqZKhP/iKNeCK7thubxLZPknHhnzMAbuUw3aVXwYg9dLTdsS2U8AFKOZf3icuHUfwekzbE1g27iq0a0yGfOrNRLeUHxvR2gCmsUaxuwftXRoD6iHWcMro8BAVThyKfBTKfWyBCDJ6TWgrDOymsaeeWi47VNf6yGZfrJF2xrvnPQT/OzvdT7pr8xTTjrFRFhysg1Sat5GDu4TH+DRLla/JnPD7TBPI2+BDaBrY3ZhWFFVCcKu5FwOyNHk9sDV3cOs8yHaYZWdbnxB0ejJJu6v2G2GsJGA8sxFbnkapEEwrH5abE3ayOrqs7/oXLhnoEuFpXp/MDPIYy4t9aP+MBlCqtgHORBgYn9qwmX50P9Q7P70VGHAzpoblMmKiWuFodDFu3WZzChPF8lR4im+Nh9tO78GGx0ycFHtvkp85U/QxaJhrg+rvbW5is0Ogl69KocBhwk+8NiQh92VEZxEcrAddHWow038nBRu/42gl+nHfQOTEyXjugaPZCK16nUrCaGMS+CN8BXVmjET+ZnVSRpnvOy0WplvKcA9r3kSPGRisratUPZXQy3VL/fbt9zQAb6XdSd5wTQGTn4PgOOqbhoMkHF1p8gZFWZOQRFTFTcQ3KkiIZQU4etOWn/+tPccHmD6NqbwWq3XaF/Pg/ZhwYQBZuEejL8IahQ4tiZE6TYELTp+HxzMKJ54tDNopgsX07Jgd+O4gUE1luQm87Qc5ci1l+lurAl74T15ileX/sBFMY4KqfHIQfRhRLQ0hrr8dRk8XmLIOBvvIUyKT/SD0XFAe2fnfw6eYx12m6FbtxDP2JNgtJE6iockPBxJxTvI09cD5M5JTTz6OAFfvnObSPIh5fQts3ojlktf5GJ+Csvpf8U1CknW3ZtU35Q8aPiuFaItidNLHGQLlvLL9jItLpOBEoMSC2rmg0irAA89pdbcZ/IG+5tr4p2lV43sQlP+E9dv6roISqA7Qha6xNLbNU7XRDdIg/yhuf3boOlsZ1eu34gwmwF1qwgppq9WVz+uCdnUJVp3QigLfVMX6i3VUbtsljb0B4WyMSTjigSC6KRXUKeuK9SBTlmUdjNAttAj2qBQ45FWy+j3XdsMtd4Mcaqi+FiW2dEjI9+p16JBCewAFv9W3rUu17AeR4P69TZd/bLzcs7KMVA/6+AS7j28FL1NX/ZiuZNMhHAGhgj8bMW7XZpJn/ZS+9pcZTv8UXxFqXB/Ig27wPfJgrVcuzsjs5vS/hXns/y85aqB977QD4xwLEKTJeCyEG1w=="
 	kmsKey       = "projects/gcp-test-195721/locations/global/keyRings/test/cryptoKeys/github_access_test_key"
 
-	installationAccessTokenEndpoint = "https://api.github.com/app/installations/1705204/access_tokens"
 	appID                           = 39801
-	expireTime                      = 10 * 60
+	clockSkewAllowance              = 5 * time.Minute
+	installationAccessTokenEndpoint = "https://api.github.com/app/installations/1705204/access_tokens"
+	signedTokenExpireTime           = 10 * 60
 )
 
 type webhookRequest struct {
@@ -56,20 +57,21 @@ type rule struct {
 }
 
 type pullRequestMessageValidator struct {
-	auth       *authenticate
+	auth       *authenticator
 	titleRules []rule
 	bodyRules  []rule
 }
 
-type authenticate struct {
+type authenticator struct {
 	endpoint string
 	jwtToken *jwt.Token
 	pk       *rsa.PrivateKey
 	token    *installationAccessToken
 }
 
-func (a *authenticate) getToken() (string, error) {
-	if a.token == nil || time.Now().After(a.token.ExpiresAt) {
+func (a *authenticator) getToken() (string, error) {
+	if err := a.token.verify(); err != nil {
+		log.Printf("Get errors when verify token: %v", err)
 		t, err := a.genInstallationAccessToken()
 		if err != nil {
 			return "", err
@@ -79,7 +81,7 @@ func (a *authenticate) getToken() (string, error) {
 	return a.token.Token, nil
 }
 
-func (a *authenticate) genInstallationAccessToken() (*installationAccessToken, error) {
+func (a *authenticator) genInstallationAccessToken() (*installationAccessToken, error) {
 	st, err := a.jwtToken.SignedString(a.pk)
 	if err != nil {
 		return nil, err
@@ -87,7 +89,7 @@ func (a *authenticate) genInstallationAccessToken() (*installationAccessToken, e
 	return a.newInstallationAccessToken(st)
 }
 
-func (a *authenticate) newInstallationAccessToken(signedToken string) (*installationAccessToken, error) {
+func (a *authenticator) newInstallationAccessToken(signedToken string) (*installationAccessToken, error) {
 	req, err := http.NewRequest(http.MethodPost, a.endpoint, nil)
 	if err != nil {
 		return nil, err
@@ -119,12 +121,37 @@ type installationAccessToken struct {
 	ExpiresAt time.Time `json:"expires_at"`
 }
 
+// notBefore returns the time when the installation access token becomes valid.
+func (iat *installationAccessToken) notBefore() time.Time {
+	return iat.ExpiresAt.Add(-1 * time.Hour)
+}
+
+// notAfter returns the time when the installation access token expires.
+func (iat *installationAccessToken) notAfter() time.Time {
+	return iat.ExpiresAt
+}
+
+// verify checks the validity of the installation access token.
+func (iat *installationAccessToken) verify() error {
+	if iat == nil {
+		return errors.New("access token: no found")
+	}
+	now := time.Now()
+	if now.Before(iat.notBefore().Add(clockSkewAllowance)) {
+		return fmt.Errorf("access token: before valid time: %v", iat.notBefore())
+	}
+	if now.After(iat.notAfter().Add(-clockSkewAllowance)) {
+		return fmt.Errorf("access token: after valid time: %v", iat.notAfter())
+	}
+	return nil
+}
+
 var v = pullRequestMessageValidator{
-	auth: &authenticate{
+	auth: &authenticator{
 		endpoint: installationAccessTokenEndpoint,
 		jwtToken: jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 			"iat": time.Now().Unix(),
-			"exp": time.Now().Unix() + expireTime,
+			"exp": time.Now().Unix() + signedTokenExpireTime,
 			"iss": appID,
 		}),
 		pk: mustGeneratePrivateKey(context.Background(), encryptedPEM),
